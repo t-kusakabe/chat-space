a0710edfc613dc8824d878d5f4d14fa08108d21f
t-kusakabe <ttmac0704@gmail.com>
2016-12-04 16:55:29 +0900

    delete context

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,17 +2,15 @@ require 'rails_helper'
 
 describe 'User' do 
   describe 'validation' do
-    context 'Is validation done?' do
-      it 'Can save users' do
-        user = build(:user)
-        expect(user).to be_valid
-      end
+    it 'Can save users' do
+      user = build(:user)
+      expect(user).to be_valid
+    end
 
-      it 'Can not save without name' do
-        user = build(:user, name: '')
-        user.valid?
-        expect(user.errors[:name].first).to include('blank')
-      end
+    it 'Can not save without name' do
+      user = build(:user, name: '')
+      user.valid?
+      expect(user.errors[:name].first).to include('blank')
     end
   end
 end
