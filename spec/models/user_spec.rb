ea114daff4e6023ee02eaf0afb6e571d2a8e360d
t-kusakabe <ttmac0704@gmail.com>
2016-12-04 16:50:10 +0900

    test contents in English

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 describe 'User' do 
   describe '#create' do
-    context 'バリデーションができているか' do
-      it 'ユーザを保存できる' do
+    context 'Is validation done?' do
+      it 'Can save users' do
         user = build(:user)
         expect(user).to be_valid
       end
 
-      it 'nameがないと保存できないか' do
+      it 'Can not save without name' do
         user = build(:user, name: '')
         user.valid?
         expect(user.errors[:name].first).to include('blank')
