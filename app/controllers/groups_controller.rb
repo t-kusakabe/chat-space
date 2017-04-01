818664b49488b8557f04d7542b0bdb51229f4273
t-kusakabe <ttmac0704@gmail.com>
2017-04-02 02:38:58 +0900

    add group_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,21 @@
 class GroupsController < ApplicationController
   def index
   end
+
+  def new
+    @group = Group.new
+    @group.group_users_through.new
+  end
+
+  def create
+    Group.create(group_params)
+
+    redirect_to group_messages_path(params[:group][:id])
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, { user_ids: [] })
+  end
 end
